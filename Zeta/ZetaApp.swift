57ec25ce28cba6bbe518f6dd68a27e3a24c37c39
Leandro Gamarra <leandrogamarra@rocketmail.com>
2021-11-21 00:50:09 -0500

    Initial Commit

---
+++ Zeta/ZetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZetaApp.swift
+//  Zeta
+//
+//  Created by Leandro Gamarra on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct ZetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
